fc77d2b2fdd93453f3f682fcfe2d19a56d67f11a
Pradeep Kumar <67951038+rpradeep02@users.noreply.github.com>
2020-10-11 20:58:35 +0530

    Add files via upload

---
+++ movies/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse ,Http404
+from django.shortcuts import render, get_object_or_404
+from .models import Movie
+# Create your views here.
+def index(request):
+    movies = Movie.objects.all()
+    #output = ', '.join([m.title for m in movies])
+    return render(request, 'movies/index.html', {'movies':movies})
+    #return HttpResponse(output) 
+    
+def detail(request, movie_id):
+    movie = get_object_or_404(Movie,pk=movie_id)
+    return render(request, 'movies/detail.html',{'movie': movie})
